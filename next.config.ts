ed68a8962b8cf0a2a689cac17fb6910be630cce0
Omar Taha <omartahads@gmail.com>
2025-02-25 06:00:00 +0200

    handle force redirect to login

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login", // Change this
+        permanent: false, // `true` for 301, `false` for 302
+      },
+    ];
+  },
 };
 
 export default nextConfig;
