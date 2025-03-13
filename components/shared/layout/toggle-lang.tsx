026c42ef0ad50f9591d5ffeec8d1b3a7b3344058
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2025-03-13 03:29:40 +0200

    Add Language Toggle

--- components/shared/layout/toggle-lang.tsx
+++ components/shared/layout/toggle-lang.tsx
@@ -1,11 +1,47 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import LangIcon from "@/public/icons/lang";
+import { useRouter, usePathname } from 'next/navigation';
+import { SA, US } from 'country-flag-icons/react/3x2'
+import { useLocale } from 'next-intl';
 
 const ToggleLang = () => {
+  const locale = useLocale();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleLocaleChange = (newLocale: string) => {
+    // Get the base path (e.g., /companies)
+    const basePath = pathname.replace(/^\/(en|ar)/, `/${newLocale}`);
+    
+    // Construct the new path with the new locale
+    const newPathname = `/${newLocale}${basePath}`;
+    
+    router.push(newPathname);
+  };
+
   return (
-    <Button size={"icon"} variant={'ghost'}>
-      <LangIcon />
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <LangIcon />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => handleLocaleChange('en')}>
+          <US style={{ width: '20px', marginRight: '5px' }} title="United States" />
+          English
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleLocaleChange('ar')}>
+          <SA style={{ width: '20px', marginRight: '5px' }} title="Saudi Arabia" />
+          Arabic
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
