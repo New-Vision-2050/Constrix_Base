a2b54f42895026143a5ed53043638c09ab3fb162
Omar Taha <omartahads@gmail.com>
2025-03-13 05:04:42 +0200

    add static row

--- app/[locale]/(main)/companies/page.tsx
+++ app/[locale]/(main)/companies/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import StatisticsRow from "@/components/shared/layout/statistics-row";
 import ExportButton from "@/modules/table/components/ExportButton";
 import TableBuilder from "@/modules/table/components/TableBuilder";
 import { CompaniesConfig } from "@/modules/table/utils/configs/companiesConfig";
@@ -8,9 +9,10 @@ import React from "react";
 const CompaniesPage = () => {
   // Get the translated config using the component
   const config = CompaniesConfig();
-  
+
   return (
-    <div className="px-8">
+    <div className="px-8 space-y-7">
+      <StatisticsRow />
       <TableBuilder
         config={config}
         searchBarActions={
---
+++ public/icons/check-static.tsx
@@ -0,0 +1,26 @@
+const CheckStatic = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="41"
+      height="40"
+      fill="none"
+      viewBox="0 0 41 40"
+    >
+      <rect
+        width="40"
+        height="40"
+        x="0.18"
+        fill="#FFE8F3"
+        fillOpacity="0.3"
+        rx="8"
+      ></rect>
+      <path
+        fill="#EF91BE"
+        d="m24.77 15.58-6.59 6.59-3.59-3.58L13.18 20l5 5 8-8zM20.18 10c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10m0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8"
+      ></path>
+    </svg>
+  );
+};
+
+export default CheckStatic;
