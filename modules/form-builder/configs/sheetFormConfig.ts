c0bf15a582d111d39809d94f6be123dbb44c4a91
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2025-03-15 15:35:38 +0200

    defaultStepHandler

--- modules/form-builder/configs/sheetFormConfig.ts
+++ modules/form-builder/configs/sheetFormConfig.ts
@@ -182,7 +182,7 @@ export const sheetFormConfig: FormConfig = {
   resetOnSuccess: true,
   
   // Enable wizard mode
-  wizard: true,
+  wizard: false,
   wizardOptions: {
     showStepIndicator: true,
     showStepTitles: true,
