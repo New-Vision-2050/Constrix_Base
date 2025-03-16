d23ed3a27c1cf0edab7d074b36d372af23fa79c6
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2025-03-16 15:10:12 +0200

    Add Create Company to table

--- app/[locale]/(main)/companies/page.tsx
+++ app/[locale]/(main)/companies/page.tsx
@@ -4,12 +4,15 @@ import ExportButton from "@/modules/table/components/ExportButton";
 import TableBuilder from "@/modules/table/components/TableBuilder";
 import { CompaniesConfig } from "@/modules/table/utils/configs/companiesConfig";
 
-import React from "react";
+import React, {useRef} from "react";
 import {SheetFormBuilder, sheetFormConfig} from "@/modules/form-builder";
 import {Button} from "@/components/ui/button";
+import CreateBuilderModule, {CreateBuilderModuleRef} from "@/features/create-builder";
 
 const CompaniesPage = () => {
-  // Get the translated config using the component
+    const companyUserModuleRef = useRef<CreateBuilderModuleRef>(null);
+
+    // Get the translated config using the component
   const config = CompaniesConfig();
 
   return (
@@ -27,6 +30,11 @@ const CompaniesPage = () => {
                   onSuccess={(values) => {
                       console.log('Form submitted successfully:', values);
                   }}
+              />
+              <CreateBuilderModule
+                  btnLabel="أنشاء شركة"
+                  moduleId={"create-company-user"}
+                  ref={companyUserModuleRef}
               />
           </div>
         }
