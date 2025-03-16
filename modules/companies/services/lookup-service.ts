f7a31320d71f3f4ff86d1820e3dcde157cfbd79a
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2025-03-16 06:47:17 +0200

    Change BaseURl and Token

--- modules/companies/services/lookup-service.ts
+++ modules/companies/services/lookup-service.ts
@@ -33,7 +33,7 @@ export const fetchCompanyFields = async () => {
   let url = apiUrl + lookupsEndPoints.countries;
   // ! The following line is a temporary override for testing purposes.
   // Remove it once the backend URL is stable and ready for production.
-  const token =`eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vY29yZS1iZS1wcjE3LmNvbnN0cml4LW52LmNvbS9hcGkvdjEvYXV0aHMvbG9naW4iLCJpYXQiOjE3NDE4MTE3MDMsImV4cCI6MTc0MTg5ODEwMywibmJmIjoxNzQxODExNzAzLCJqdGkiOiI0V2puUzJITVBxUGJtdzNPIiwic3ViIjoiYzcxMTkxYjUtZWJjZS00ZmQxLTlhNjgtYzIwOTljMmEzZjM5IiwicHJ2IjoiYmI2NWQ5YjhmYmYwZGE5ODI3YzhlZDIzMWQ5YzU0YzgxN2YwZmJiMiJ9.Dk1YdDd03FOWfrCfKeNNdNG7KlI0oy6ri4xCx3qznvU`;
+    const token = getCookie("new-vision-token");
   url = `https://core-be-stage.constrix-nv.com/api/v1${lookupsEndPoints.companyFields}`;
 
   const response = await axios.get<fetchCompanyFieldsResponseT>(url, {
