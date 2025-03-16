2e73d2195038c7ead573e7dd7cf0b1c1dc20fdcd
SallamRady <sallamrady@gmail.com>
2025-03-16 17:32:20 +0300

    add create company - user module in companies page

--- app/[locale]/(main)/companies/page.tsx
+++ app/[locale]/(main)/companies/page.tsx
@@ -5,8 +5,9 @@ import TableBuilder from "@/modules/table/components/TableBuilder";
 import { CompaniesConfig } from "@/modules/table/utils/configs/companiesConfig";
 
 import React from "react";
-import {SheetFormBuilder, sheetFormConfig} from "@/modules/form-builder";
-import {Button} from "@/components/ui/button";
+import { SheetFormBuilder, sheetFormConfig } from "@/modules/form-builder";
+import { Button } from "@/components/ui/button";
+import CreateBuilderModule from "@/features/create-builder";
 
 const CompaniesPage = () => {
   // Get the translated config using the component
@@ -16,18 +17,23 @@ const CompaniesPage = () => {
     <div className="px-8 space-y-7">
       <StatisticsRow />
 
+      <CreateBuilderModule
+        btnLabel="أنشاء شركة"
+        moduleId={"create-company-user"}
+      />
+
       <TableBuilder
         config={config}
         searchBarActions={
           <div>
             <ExportButton data={["omar"]} />
-              <SheetFormBuilder
-                  config={sheetFormConfig}
-                  trigger={<Button>Open Form</Button>}
-                  onSuccess={(values) => {
-                      console.log('Form submitted successfully:', values);
-                  }}
-              />
+            <SheetFormBuilder
+              config={sheetFormConfig}
+              trigger={<Button>Open Form</Button>}
+              onSuccess={(values) => {
+                console.log("Form submitted successfully:", values);
+              }}
+            />
           </div>
         }
       />
