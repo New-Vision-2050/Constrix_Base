2d05192c608737430ff17f1f167222f8b9517891
SallamRady <sallamrady@gmail.com>
2025-03-24 00:41:41 +0300

    implement LoginIdentifierStatus ui

--- modules/settings/components/tabs/IdentifierSetting/tabs/IdentifierTab/components/table/LoginIdentifierStatus.tsx
+++ modules/settings/components/tabs/IdentifierSetting/tabs/IdentifierTab/components/table/LoginIdentifierStatus.tsx
@@ -1,9 +1,32 @@
 import { Switch } from "@/components/ui/switch";
+import { apiClient } from "@/config/axios-config";
 import { LoginIdentifier } from "@/modules/settings/types/LoginIdentifier";
+import { useTableInstance } from "@/modules/table/store/useTableStore";
+import { toast } from "sonner";
 
 type PropsT = {
   identifier: LoginIdentifier;
 };
 export default function LoginIdentifierStatus({ identifier }: PropsT) {
-  return <Switch />;
+  const { reloadTable } = useTableInstance("login-identifier-table");
+  const handleChangeStatus = async () => {
+    try {
+      const response = await apiClient.post(
+        `settings/identifier/make-default/${identifier.id}`
+      );
+      console.log("response-response", response);
+      reloadTable();
+      toast.success("Login way status changed successfully");
+    } catch (error) {
+      console.log("error in change login way status ::", error);
+    }
+  };
+
+  console.log("identifieridentifier", identifier);
+  return (
+    <Switch
+      checked={identifier.status == 1}
+      onCheckedChange={handleChangeStatus}
+    />
+  );
 }
