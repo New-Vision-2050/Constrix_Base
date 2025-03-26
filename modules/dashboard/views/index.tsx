960b424c881435d406155f10d3ad28338a08f922
SallamRady <sallamrady@gmail.com>
2025-03-27 00:26:41 +0300

    implement empty card component

--- modules/dashboard/views/index.tsx
+++ modules/dashboard/views/index.tsx
@@ -1,12 +1,13 @@
 import UserProfileHeader from "../components/profile-header";
+import StatisticsCardsSection from "../components/statistics-cards";
 
 export default function UserDashboardModule() {
   return (
     <div className="flex flex-col gap-12 container p-12">
       {/* header */}
       <UserProfileHeader />
-      {/* row 2 */}
-      <div className="flex">Profile Content</div>
+      {/* Statistics cards */}
+      <StatisticsCardsSection />
     </div>
   );
 }
