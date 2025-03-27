e4eae3a6872fa7d2e848d40c6dee7dbee7836d36
SallamRady <sallamrady@gmail.com>
2025-03-27 03:23:37 +0300

    implement card layout

---
+++ modules/dashboard/components/UserInformationCardLayout.tsx
@@ -0,0 +1,21 @@
+import InfoIcon from "@/public/icons/InfoIcon";
+import SettingsIcon from "@/public/icons/settings";
+
+type PropsT = {
+  title: string;
+  children: React.ReactNode;
+};
+export default function UserInformationCardLayout({ title, children }: PropsT) {
+  return (
+    <div className="w-full flex gap-4 flex-col p-4 bg-sidebar rounded-lg shadow-md">
+      <div className="flex items-center justify-between">
+        <div className="flex gap-1">
+          <InfoIcon additionClass="text-orange-400" />
+          <p className="font-bold text-lg">{title}</p>
+        </div>
+        <SettingsIcon />
+      </div>
+      <div className="flex-grow">{children}</div>
+    </div>
+  );
+}
