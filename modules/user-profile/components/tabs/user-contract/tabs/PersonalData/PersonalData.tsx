244348e7652297c94064dd86751cfa1034185639
SallamRady <sallamrady@gmail.com>
2025-04-08 16:39:24 +0300

    implement connection section of data

--- modules/user-profile/components/tabs/user-contract/tabs/PersonalData/PersonalData.tsx
+++ modules/user-profile/components/tabs/user-contract/tabs/PersonalData/PersonalData.tsx
@@ -1,6 +1,7 @@
 import VerticalBtnsList from "../components/vertical-buttons-list";
-import BankingDataSection from "./components/BankingDataSection";
 // import PersonalDataSection from "./components/PersonalDataSection";
+// import BankingDataSection from "./components/BankingDataSection";
+import ConnectionDataSection from "./components/connectionDataSection";
 import { PersonalDataVerticalButtons } from "./constants/VerticalBtns";
 
 export default function PersonalDataTab() {
@@ -9,7 +10,8 @@ export default function PersonalDataTab() {
       <VerticalBtnsList items={PersonalDataVerticalButtons} />
       <div className="p-4 flex-grow gap-8">
         {/* <PersonalDataSection /> */}
-        <BankingDataSection />
+        {/* <BankingDataSection /> */}
+        <ConnectionDataSection />
       </div>
     </div>
   );
