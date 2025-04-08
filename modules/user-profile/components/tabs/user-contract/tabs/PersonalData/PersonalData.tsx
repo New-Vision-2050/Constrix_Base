b7ad3e7078cb68e0fc19163a505369b05af76e38
SallamRady <sallamrady@gmail.com>
2025-04-08 10:15:44 +0300

    Vertical Btns List

--- modules/user-profile/components/tabs/user-contract/tabs/PersonalData/PersonalData.tsx
+++ modules/user-profile/components/tabs/user-contract/tabs/PersonalData/PersonalData.tsx
@@ -1,45 +1,10 @@
-import GraduationCapIcon from "@/public/icons/graduation-cap";
-import InfoIcon from "@/public/icons/InfoIcon";
+import VerticalBtnsList from "../components/vertical-buttons-list";
+import { PersonalDataVerticalButtons } from "./constants/VerticalBtns";
 
 export default function PersonalDataTab() {
   return (
     <div className="flex gap-8">
-      <div className="w-[200px] p-4 m-2 flex flex-col gap-4 bg-sidebar rounded-md shadow-md">
-        
-        {/* item */}
-        <div className="w-full flex items-center justify-around cursor-pointer">
-          <div className="flex gap-2">
-            <GraduationCapIcon />
-            <p className="text-md font-semibold">test</p>
-          </div>
-          <InfoIcon additionClass="text-orange-500" />
-        </div>
-        {/* item */}
-        <div className="w-full flex items-center justify-around cursor-pointer">
-          <div className="flex gap-2">
-            <GraduationCapIcon />
-            <p className="text-md font-semibold">test</p>
-          </div>
-          <InfoIcon additionClass="text-orange-500" />
-        </div>
-        {/* item */}
-        <div className="w-full flex items-center justify-around cursor-pointer">
-          <div className="flex gap-2">
-            <GraduationCapIcon />
-            <p className="text-md font-semibold">test</p>
-          </div>
-          <InfoIcon additionClass="text-orange-500" />
-        </div>
-        {/* item */}
-        <div className="w-full flex items-center justify-around cursor-pointer">
-          <div className="flex gap-2">
-            <GraduationCapIcon />
-            <p className="text-md font-semibold">test</p>
-          </div>
-          <InfoIcon additionClass="text-orange-500" />
-        </div>
-
-      </div>
+      <VerticalBtnsList items={PersonalDataVerticalButtons} />
       <span>personal data</span>
     </div>
   );
