883639cf3804820bb398fc77f3cf98e559bfc754
SallamRady <sallamrady@gmail.com>
2025-04-20 11:27:42 +0300

    handle case there is no data in user profile tabs

--- modules/user-profile/components/tabs/user-contract/tabs/AcademicAndExperience/components/content-manager/components/courses/UserCoursesList.tsx
+++ modules/user-profile/components/tabs/user-contract/tabs/AcademicAndExperience/components/content-manager/components/courses/UserCoursesList.tsx
@@ -2,9 +2,21 @@ import RegularList from "@/components/shared/RegularList";
 import { useUserAcademicTabsCxt } from "../UserAcademicTabsCxt";
 import SingleCourse from "./single-course";
 import { Course } from "@/modules/user-profile/types/Course";
+import NoDataFounded from "@/modules/user-profile/components/NoDataFounded";
 
 export default function UserCoursesList() {
   const { userCourses } = useUserAcademicTabsCxt();
+
+  // handle there is no data found
+  if (userCourses && userCourses.length === 0)
+    return (
+      <NoDataFounded
+        title="لا يوجد بيانات"
+        subTitle="لا يوجد بيانات تخص الكورسات للمستخدم قم باضافة كورس / دورة"
+      />
+    );
+
+  // render data
   return (
     <RegularList<Course, "course">
       sourceName="course"
--- modules/user-profile/components/tabs/user-contract/tabs/FinancialData/components/privileges-and-allowances/PrivilegesFormsList.tsx
+++ modules/user-profile/components/tabs/user-contract/tabs/FinancialData/components/privileges-and-allowances/PrivilegesFormsList.tsx
@@ -1,9 +1,20 @@
+import NoDataFounded from "@/modules/user-profile/components/NoDataFounded";
 import { useFinancialDataCxt } from "../../context/financialDataCxt";
 import PrivilegeItem from "./components/privilege-item";
 
 export default function PrivilegesFormsList() {
   const { addedPrivilegesList } = useFinancialDataCxt();
 
+  // handle there is no data found
+  if (addedPrivilegesList && addedPrivilegesList.length === 0)
+    return (
+      <NoDataFounded
+        title="لا يوجد بيانات"
+        subTitle="لا يوجد بدلات للمستخدم قم باضافة بدل جديد"
+      />
+    );
+
+  // render data
   return (
     <div className="flex flex-col gap-8">
       {addedPrivilegesList &&
