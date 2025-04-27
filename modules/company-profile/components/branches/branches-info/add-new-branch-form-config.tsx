c0e3c033cddce7517791177b4ee3dcf4328a6a8f
Omar Taha <omartahads@gmail.com>
2025-04-28 00:28:26 +0300

    handle in_general in pickup map location

--- modules/company-profile/components/branches/branches-info/add-new-branch-form-config.tsx
+++ modules/company-profile/components/branches/branches-info/add-new-branch-form-config.tsx
@@ -189,6 +189,7 @@ export const addNewBranchFormConfig = (branches: Branch[]) => {
                   "latitude",
                   "longitude",
                 ]}
+                inGeneral={true}
               />
             ),
           },
