10cbdda8e3f06fd4c111ab23ebdb2ca44ebbbcae
SallamRady <sallamrady@gmail.com>
2025-04-27 23:16:04 +0300

    sync tab1 and tab2 states with URL search params to preserve user selection on page reload

--- modules/user-profile/components/tabs/user-contract/index.tsx
+++ modules/user-profile/components/tabs/user-contract/index.tsx
@@ -5,12 +5,15 @@ import { useUserProfileCxt } from "@/modules/user-profile/context/user-profile-c
 
 export default function UserContractTab() {
   // declare and define component state and variables.
-  const { tab2 } = useUserProfileCxt();
+  const { tab2, setTab2 } = useUserProfileCxt();
   const t = useTranslations("UserProfile.tabs.contractTabs");
   // declare and define component helper methods.
   // return component ui.
   return (
     <HorizontalTabs
+      onTabClick={(tab) => {
+        setTab2(tab.id);
+      }}
       list={GetUserContractTabsList(t)}
       defaultValue={tab2 !== null ? tab2 : undefined}
     />
