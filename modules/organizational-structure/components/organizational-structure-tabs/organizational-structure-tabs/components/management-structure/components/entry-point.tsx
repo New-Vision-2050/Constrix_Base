44c6a5a38312795b30fbd61eb3ce6f2ee7629ed4
SallamRady <sallamrady@gmail.com>
2025-05-05 17:39:29 +0300

    search bar in management tab

--- modules/organizational-structure/components/organizational-structure-tabs/organizational-structure-tabs/components/management-structure/components/entry-point.tsx
+++ modules/organizational-structure/components/organizational-structure-tabs/organizational-structure-tabs/components/management-structure/components/entry-point.tsx
@@ -3,11 +3,12 @@
 import ReactFlowDiagram from "./ReactFlowDiagram";
 import VerticalBtnsList from "@/components/shared/VerticalList";
 import { CompanyStructureBranch } from "../../company-structure/constants/BranchiesVerticalBtnsList";
+import ManagementStructureSearchBar from "./ManagementStructureSearchBar";
 
 export default function CompanyStructureEntryPoint() {
   return (
     <div className="flex flex-col gap-7">
-      <>Tab Header</>
+      <ManagementStructureSearchBar />
       <div className="flex gap-8">
         <VerticalBtnsList items={CompanyStructureBranch} />
         <div className="p-4 flex-grow gap-8">
