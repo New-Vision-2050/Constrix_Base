dbc49f54a0c97a868ada8636c7e1bf52a829e4e6
Mohamed Abou Hagar <13303169+mabou7agar@users.noreply.github.com>
2025-05-07 01:25:13 +0300

    Revert "Revert "Feature/org chart json flow (#85)""

---
+++ modules/organizational-structure/components/StatisticsCard/StatisticsCardHeader.tsx
@@ -0,0 +1,33 @@
+type PropsT = {
+  title?: string;
+  number?: string;
+  description?: string;
+  icon?: JSX.Element;
+};
+
+export default function StatisticsCardHeader(props: PropsT) {
+  const { title, icon, description, number } = props;
+
+  return (
+    <div className="flex items-center gap-2">
+      {/* icon */}
+      <div
+        className="w-10 h-10 rounded-lg flex items-center justify-center"
+        style={{
+          background: `linear-gradient(0deg, var(--Buttons-States-Green-Background, #38484A), var(--Buttons-States-Green-Background, #38484A)),
+                 linear-gradient(0deg, rgba(48, 51, 78, 0.88), rgba(48, 51, 78, 0.88))`,
+        }}
+      >
+        {icon}
+      </div>
+      {/* title & description */}
+      <div>
+        <p className="text-lg font-bold">{title}</p>
+        <p className="text-sm font-thin">
+          <span className="text-lg font-bold">{number}</span>
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+}
