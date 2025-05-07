4620ece7c9bd442b1d707ecc3dc2595a6ede99e5
Omar Taha <omartahads@gmail.com>
2025-05-07 03:12:23 +0300

    handle dynamic row field delete element in legal data form

--- modules/company-profile/components/official-data/legal-data-section/legal-data-form-config.tsx
+++ modules/company-profile/components/official-data/legal-data-section/legal-data-form-config.tsx
@@ -96,6 +96,12 @@ export const LegalDataFormConfig = (
               ],
               maxRows: 10,
               columns: 1,
+              deleteUrl: `${baseURL}/companies/company-profile/legal-data`,
+              onDeleteSuccess: () => {
+                queryClient.refetchQueries({
+                  queryKey: ["main-company-data", id, company_id],
+                });
+              },
             },
           },
         ],
