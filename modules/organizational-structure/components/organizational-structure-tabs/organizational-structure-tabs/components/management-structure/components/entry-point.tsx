5ec4bbf30dc41233c13938d9b4ff6e132996c5ac
SallamRady <sallamrady@gmail.com>
2025-05-11 11:30:17 +0300

    handle add company management

--- modules/organizational-structure/components/organizational-structure-tabs/organizational-structure-tabs/components/management-structure/components/entry-point.tsx
+++ modules/organizational-structure/components/organizational-structure-tabs/organizational-structure-tabs/components/management-structure/components/entry-point.tsx
@@ -2,15 +2,41 @@
 
 import ReactFlowDiagram from "./ReactFlowDiagram";
 import VerticalBtnsList from "@/components/shared/VerticalList";
-import { CompanyStructureBranch } from "../../company-structure/constants/BranchiesVerticalBtnsList";
 import ManagementStructureSearchBar from "./ManagementStructureSearchBar";
+import { useOrgStructureCxt } from "@/modules/organizational-structure/context/OrgStructureCxt";
+import { UserProfileNestedTab } from "@/modules/user-profile/types/user-profile-nested-tabs-content";
+import { useEffect, useState } from "react";
+import { MapPin } from "lucide-react";
+import { useManagementsStructureCxt } from "../context";
 
 export default function CompanyStructureEntryPoint() {
+  const { branchiesList } = useOrgStructureCxt();
+  const { handleChangeActiveBranch } = useManagementsStructureCxt();
+  const [items, setItems] = useState<UserProfileNestedTab[]>([]);
+
+  // ** handle side effects
+  useEffect(() => {
+    if (branchiesList) {
+      const _items = branchiesList?.map((branch) => ({
+        id: branch.id,
+        title: branch.name,
+        icon: <MapPin />,
+        content: <>{branch.name}</>,
+        ignoreValidation: true,
+        onClick() {
+          handleChangeActiveBranch(branch.id);
+        },
+      }));
+      if (_items.length) handleChangeActiveBranch(_items[0]?.id);
+      setItems(_items);
+    }
+  }, [branchiesList]);
+
   return (
     <div className="flex flex-col gap-7">
       <ManagementStructureSearchBar />
       <div className="flex gap-8">
-        <VerticalBtnsList items={CompanyStructureBranch} />
+        <VerticalBtnsList items={items} />
         <div className="p-4 flex-grow gap-8">
           <ReactFlowDiagram />
         </div>
