15013aa0cae91a822f586eff47f279f94a306ed1
Mohamed Abdel Sattar <abdel.sattar.bombo@gmail.com>
2025-05-15 00:58:53 +0300

    fix: remove importing error

--- modules/organizational-structure/constants/organizational-structure-tabs.tsx
+++ modules/organizational-structure/constants/organizational-structure-tabs.tsx
@@ -2,8 +2,7 @@ import { SystemTab } from "@/modules/settings/types/SystemTab";
 import BackpackIcon from "@/public/icons/backpack";
 import { LayoutDashboardIcon, UserIcon } from "lucide-react";
 import CompanyOrganizationStructure from "@/modules/organizational-structure/components/company-organization-structure";
-import ManagementsStructure from "../components/organizational-structure-tabs/organizational-structure-tabs/components/management-structure";
-import CompanyManagementsStructure from '@/modules/organizational-structure/components/company-management-structure'
+import CompanyManagementsStructure from '@/modules/organizational-structure/components/company-management-structure';
 
 export const OrganizationalStructureSubTabs: SystemTab[] = [
   {
