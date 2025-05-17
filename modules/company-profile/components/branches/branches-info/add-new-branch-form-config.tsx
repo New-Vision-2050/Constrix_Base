2ce02fb6179113398027b7af8a97119031ed05fb
Omar Taha <omartahads@gmail.com>
2025-05-17 06:21:27 +0300

    change pickup address path

--- modules/company-profile/components/branches/branches-info/add-new-branch-form-config.tsx
+++ modules/company-profile/components/branches/branches-info/add-new-branch-form-config.tsx
@@ -1,7 +1,7 @@
 import { FormConfig } from "@/modules/form-builder";
 import { baseURL } from "@/config/axios-config";
 import { Branch } from "@/modules/company-profile/types/company";
-import PickupMap from "../../official-data/national-address/pickup-map";
+import PickupMap from "../../../../../components/shared/pickup-map";
 import { useQueryClient } from "@tanstack/react-query";
 import { defaultSubmitHandler } from "@/modules/form-builder/utils/defaultSubmitHandler";
 import { useParams } from "next/navigation";
