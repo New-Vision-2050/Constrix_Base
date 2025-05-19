506154f283fde8cdbcfa88c15f24b3b2671596a4
SallamRady <sallamrady@gmail.com>
2025-05-19 11:58:12 +0300

    set translation in Org Structure

--- modules/organizational-structure/constants/organizational-structure-settings.tsx
+++ modules/organizational-structure/constants/organizational-structure-settings.tsx
@@ -3,10 +3,12 @@ import { UserIcon } from "lucide-react";
 import OrgStructureJobTitlesSetting from "../components/organizational-structure-tabs/organizational-structure-settings/components/org-structure-job-titles";
 import OrgStructureJobTypesSetting from "../components/organizational-structure-tabs/organizational-structure-settings/components/org-structure-job-types";
 
-export const OrganizationalStructureSettingsTabs: SystemTab[] = [
+export const OrganizationalStructureSettingsTabs = (
+  t: (key: string) => string
+): SystemTab[] => [
   {
     id: "organizational-structure-settings-job-titles",
-    title: "المسميات الوظيفية",
+    title: t("jobTitles.title"),
     icon: <UserIcon />,
     content: (
       <>
@@ -16,7 +18,7 @@ export const OrganizationalStructureSettingsTabs: SystemTab[] = [
   },
   {
     id: "organizational-structure-settings-job-types",
-    title: "انواع الوظائف",
+    title: t("jobTypes.title"),
     icon: <UserIcon />,
     content: <OrgStructureJobTypesSetting />,
   },
