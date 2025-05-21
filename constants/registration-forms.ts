3fbe567e1cc2dafde1078c5619ffb990fde4a127
Omar Taha <omartahads@gmail.com>
2025-05-21 04:46:04 +0300

    apply employee table config

--- constants/registration-forms.ts
+++ constants/registration-forms.ts
@@ -1,6 +1,8 @@
 import { brokerFormConfig } from "@/modules/form-builder/configs/brokerFormConfig";
 import { customerFormConfig } from "@/modules/form-builder/configs/customerFormConfig";
 import { employeeFormConfig } from "@/modules/form-builder/configs/employeeFormConfig";
+import { EmployeeTableConfig } from "@/modules/table/config/EmployeeTableConfig";
+import { UsersConfig } from "@/modules/table/utils/configs/usersTableConfig";
 
 export const REGISTRATION_FORMS_SLUGS = {
   EMPLOYEE: "employee",
@@ -12,4 +14,11 @@ export const REGISTRATION_FORMS = {
   [REGISTRATION_FORMS_SLUGS.EMPLOYEE]: employeeFormConfig,
   [REGISTRATION_FORMS_SLUGS.CUSTOMER]: customerFormConfig,
   [REGISTRATION_FORMS_SLUGS.RESELLER]: brokerFormConfig,
+};
+
+export const REGISTRATION_TABLES = {
+  [REGISTRATION_FORMS_SLUGS.EMPLOYEE]: EmployeeTableConfig,
+  [REGISTRATION_FORMS_SLUGS.CUSTOMER]: UsersConfig,
+  [REGISTRATION_FORMS_SLUGS.RESELLER]: UsersConfig,
+
 };
