86fb16a9ae29e0209fbfcc2ded35b28b65537a8f
SallamRady <sallamrady@gmail.com>
2025-06-25 14:28:00 +0300

    feat: add attendance departure table component with map button

--- modules/attendance-departure/components/AttendanceDepartureTable.tsx
+++ modules/attendance-departure/components/AttendanceDepartureTable.tsx
@@ -2,11 +2,19 @@
 import React from "react";
 import TableBuilder from "@/modules/table/components/TableBuilder";
 import { getAttendanceDepartureTableConfig } from "../AttendanceDepartureTableConfig";
+import { Button } from "@/components/ui/button";
 
 const AttendanceDepartureTable: React.FC = () => {
   return (
     <div className="mt-4">
-      <TableBuilder config={getAttendanceDepartureTableConfig()} />
+      <TableBuilder 
+      config={getAttendanceDepartureTableConfig()} 
+      searchBarActions={
+        <div className="flex items-center gap-3">
+          <Button>الخريطة</Button>
+        </div>
+      }
+      />
     </div>
   );
 };
