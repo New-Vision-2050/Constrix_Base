c401d71c3139b38d2b46fe288dbe12f71866468b
SallamRady <sallamrady@gmail.com>
2025-07-02 14:45:42 +0300

    feat: implement organization chart with tree and list view components

--- modules/organizational-structure/org-chart/components/organization-chart/chart-add-button.tsx
+++ modules/organizational-structure/org-chart/components/organization-chart/chart-add-button.tsx
@@ -5,10 +5,12 @@ import { OrgChartNode } from "@/types/organization";
 type PropsT = {
   node: OrgChartNode;
   onAddBtnClick?: (node: OrgChartNode) => void;
+  ignoreAddAtFirstNode?: boolean;
 };
 
-export default function OrgChartAddButton({ node, onAddBtnClick }: PropsT) {
-  if (!onAddBtnClick) return <></>;
+export default function OrgChartAddButton({ node, onAddBtnClick, ignoreAddAtFirstNode }: PropsT) {
+  // Check if the button should be hidden
+  if (!onAddBtnClick || (ignoreAddAtFirstNode && node.is_main === 1)) return <></>;
   return (
     <div className="relative w-full h-8">
       <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
