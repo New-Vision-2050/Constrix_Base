b9d64eff1ae492cd95bf8d4a07a5a43cc4b7bf98
Omar Taha <omartahads@gmail.com>
2025-07-10 03:30:57 +0300

    use UsersContent as a child for CanSeeContent

--- app/[locale]/(main)/users/page.tsx
+++ app/[locale]/(main)/users/page.tsx
@@ -1,35 +1,17 @@
 "use client";
-import StatisticsRow from "@/components/shared/layout/statistics-row";
-import { Button } from "@/components/ui/button";
-import { SheetFormBuilder } from "@/modules/form-builder";
-import TableBuilder from "@/modules/table/components/TableBuilder";
-import { UsersConfig } from "@/modules/table/utils/configs/usersTableConfig";
-import { statisticsConfig } from "@/modules/users/components/statistics-config";
-import React from "react";
-import { GetCompanyUserFormConfig } from "@/modules/form-builder/configs/companyUserFormConfig";
-import {useTranslations} from "next-intl";
+
+import { can } from "@/hooks/useCan";
+import CanSeeContent from "@/components/shared/CanSeeContent";
+import UsersContent from "@/modules/users/components/UsersContent";
+import { Actions } from "@/lib/ability";
 
 const UsersPage = () => {
-  const config = UsersConfig();
-    const t = useTranslations('Companies');
+  const permission = can(["list"], "users.user") as Record<Actions, boolean>;
+  
   return (
-    <div className="px-8 space-y-7">
-      <StatisticsRow config={statisticsConfig} />{" "}
-      <TableBuilder
-        config={config}
-        searchBarActions={
-          <div className="flex items-center gap-3">
-            <SheetFormBuilder
-              config={GetCompanyUserFormConfig(t)}
-              trigger={<Button>إنشاء مستخدم</Button>}
-              onSuccess={(values) => {
-                console.log("Form submitted successfully:", values);
-              }}
-            />{" "}
-          </div>
-        }
-      />
-    </div>
+    <CanSeeContent canSee={permission.list}>
+      <UsersContent />
+    </CanSeeContent>
   );
 };
 
