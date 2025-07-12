ebfcc4766e241e5fe1c29630572eb4781407bd94
Omar Taha <omartahads@gmail.com>
2025-07-13 02:25:29 +0300

    update practical-permissions

--- modules/roles-and-permissions/examples/practical-permissions.tsx
+++ modules/roles-and-permissions/examples/practical-permissions.tsx
@@ -8,12 +8,12 @@ export function CompaniesPagePermissions() {
   const { can } = useCan();
 
   // Check permissions for companies
-  const canListCompanies = can('LIST', 'COMPANY');
-  const canCreateCompany = can('CREATE', 'COMPANY');
-  const canUpdateCompany = can('UPDATE', 'COMPANY');
-  const canDeleteCompany = can('DELETE', 'COMPANY');
-  const canExportCompanies = can('EXPORT', 'COMPANY');
-  const canViewCompany = can('VIEW', 'COMPANY');
+  const canListCompanies = can(PERMISSION_ACTIONS.LIST, PERMISSION_SUBJECTS.COMPANY);
+  const canCreateCompany = can(PERMISSION_ACTIONS.CREATE, PERMISSION_SUBJECTS.COMPANY);
+  const canUpdateCompany = can(PERMISSION_ACTIONS.UPDATE, PERMISSION_SUBJECTS.COMPANY);
+  const canDeleteCompany = can(PERMISSION_ACTIONS.DELETE, PERMISSION_SUBJECTS.COMPANY);
+  const canExportCompanies = can(PERMISSION_ACTIONS.EXPORT, PERMISSION_SUBJECTS.COMPANY);
+  const canViewCompany = can(PERMISSION_ACTIONS.VIEW, PERMISSION_SUBJECTS.COMPANY);
 
   return {
     canListCompanies,
@@ -77,11 +77,11 @@ export function UserProfilePermissions() {
   const { can } = useCan();
   
   return {
-    canUpdateUserData: can('UPDATE', 'USER_PROFILE_DATA'),
-    canViewUserContact: can('VIEW', 'USER_PROFILE_CONTACT'),
-    canUpdateUserContact: can('UPDATE', 'USER_PROFILE_CONTACT'),
-    canViewUserIdentity: can('VIEW', 'USER_PROFILE_IDENTITY'),
-    canUpdateUserIdentity: can('UPDATE', 'USER_PROFILE_IDENTITY'),
+    canUpdateUserData: can(PERMISSION_ACTIONS.UPDATE, PERMISSION_SUBJECTS.USER_PROFILE_DATA),
+    canViewUserContact: can(PERMISSION_ACTIONS.VIEW, PERMISSION_SUBJECTS.USER_PROFILE_CONTACT),
+    canUpdateUserContact: can(PERMISSION_ACTIONS.UPDATE, PERMISSION_SUBJECTS.USER_PROFILE_CONTACT),
+    canViewUserIdentity: can(PERMISSION_ACTIONS.VIEW, PERMISSION_SUBJECTS.USER_PROFILE_IDENTITY),
+    canUpdateUserIdentity: can(PERMISSION_ACTIONS.UPDATE, PERMISSION_SUBJECTS.USER_PROFILE_IDENTITY),
   };
 }
 
@@ -90,11 +90,11 @@ export function RolesPermissions() {
   const { can } = useCan();
   
   return {
-    canListRoles: can('LIST', 'ROLE'),
-    canCreateRole: can('CREATE', 'ROLE'),
-    canUpdateRole: can('UPDATE', 'ROLE'),
-    canDeleteRole: can('DELETE', 'ROLE'),
-    canViewRole: can('VIEW', 'ROLE'),
+    canListRoles: can(PERMISSION_ACTIONS.LIST, PERMISSION_SUBJECTS.ROLE),
+    canCreateRole: can(PERMISSION_ACTIONS.CREATE, PERMISSION_SUBJECTS.ROLE),
+    canUpdateRole: can(PERMISSION_ACTIONS.UPDATE, PERMISSION_SUBJECTS.ROLE),
+    canDeleteRole: can(PERMISSION_ACTIONS.DELETE, PERMISSION_SUBJECTS.ROLE),
+    canViewRole: can(PERMISSION_ACTIONS.VIEW, PERMISSION_SUBJECTS.ROLE),
   };
 }
 
@@ -103,11 +103,11 @@ export function UsersPermissions() {
   const { can } = useCan();
   
   return {
-    canListUsers: can('LIST', 'USER'),
-    canCreateUser: can('CREATE', 'USER'),
-    canUpdateUser: can('UPDATE', 'USER'),
-    canDeleteUser: can('DELETE', 'USER'),
-    canViewUser: can('VIEW', 'USER'),
-    canExportUsers: can('EXPORT', 'USER'),
+    canListUsers: can(PERMISSION_ACTIONS.LIST, PERMISSION_SUBJECTS.USER),
+    canCreateUser: can(PERMISSION_ACTIONS.CREATE, PERMISSION_SUBJECTS.USER),
+    canUpdateUser: can(PERMISSION_ACTIONS.UPDATE, PERMISSION_SUBJECTS.USER),
+    canDeleteUser: can(PERMISSION_ACTIONS.DELETE, PERMISSION_SUBJECTS.USER),
+    canViewUser: can(PERMISSION_ACTIONS.VIEW, PERMISSION_SUBJECTS.USER),
+    canExportUsers: can(PERMISSION_ACTIONS.EXPORT, PERMISSION_SUBJECTS.USER),
   };
 }
