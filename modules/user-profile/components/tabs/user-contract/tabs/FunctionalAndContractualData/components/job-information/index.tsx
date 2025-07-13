400d2c5d0d239eb2da5039f1157b51c4c98ce023
Omar Taha <omartahads@gmail.com>
2025-07-13 06:29:34 +0300

    handle ACL for EMPLOYMENT_INFO - view and update

--- modules/user-profile/components/tabs/user-contract/tabs/FunctionalAndContractualData/components/job-information/index.tsx
+++ modules/user-profile/components/tabs/user-contract/tabs/FunctionalAndContractualData/components/job-information/index.tsx
@@ -2,19 +2,28 @@ import { useFunctionalContractualCxt } from "../../context";
 import JobInformationEditMode from "./JobInformationEditMode";
 import JobInformationPreviewMode from "./JobInformationPreviewMode";
 import TabTemplate from "@/components/shared/TabTemplate/TabTemplate";
+import { can } from "@/hooks/useCan";
+import { PERMISSION_ACTIONS, PERMISSION_SUBJECTS } from "@/modules/roles-and-permissions/permissions";
+import CanSeeContent from "@/components/shared/CanSeeContent";
 
 export default function JobInformation() {
   const { professionalDataLoading } = useFunctionalContractualCxt();
-  return (
-    <div className="p-4 flex-grow flex flex-col gap-12">
-      <p className="text-lg font-bold">البيانات الوظيفية</p>
+  const canView = can(PERMISSION_ACTIONS.VIEW, PERMISSION_SUBJECTS.EMPLOYMENT_INFO) as boolean;
+  const canUpdate = can(PERMISSION_ACTIONS.UPDATE, PERMISSION_SUBJECTS.EMPLOYMENT_INFO) as boolean;
 
-      <TabTemplate
-        title={"بيانات التوظيف"}
-        loading={professionalDataLoading}
-        reviewMode={<JobInformationPreviewMode />}
-        editMode={<JobInformationEditMode />}
-      />
-    </div>
+  return (
+    <CanSeeContent canSee={canView}>
+      <div className="p-4 flex-grow flex flex-col gap-12">
+        <p className="text-lg font-bold">البيانات الوظيفية</p>
+
+        <TabTemplate
+          title={"بيانات التوظيف"}
+          loading={professionalDataLoading}
+          reviewMode={<JobInformationPreviewMode />}
+          editMode={<JobInformationEditMode />}
+          canEdit={canUpdate}
+        />
+      </div>
+    </CanSeeContent>
   );
 }
