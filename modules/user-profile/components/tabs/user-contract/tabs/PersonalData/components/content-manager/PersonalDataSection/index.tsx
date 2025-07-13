fe94ccc0f9c3d66bdf742b61d720d964421d285f
Omar Taha <omartahads@gmail.com>
2025-07-13 04:27:44 +0300

    handle ACL for USER_PROFILE:  DATA, CONTACT and IDENTITY view

--- modules/user-profile/components/tabs/user-contract/tabs/PersonalData/components/content-manager/PersonalDataSection/index.tsx
+++ modules/user-profile/components/tabs/user-contract/tabs/PersonalData/components/content-manager/PersonalDataSection/index.tsx
@@ -3,19 +3,32 @@ import ConnectionDataSectionPersonalForm from "./components/connection-user-data
 import IdentityDataSectionPersonalForm from "./components/Identity-user-data";
 import PassportDataSectionPersonalForm from "./components/passport-user-data";
 import { useUserProfileCxt } from "@/modules/user-profile/context/user-profile-cxt";
+import { can } from "@/hooks/useCan";
+import { PERMISSION_ACTIONS, PERMISSION_SUBJECTS } from "@/modules/roles-and-permissions/permissions";
 
 export default function PersonalDataSection() {
+  const permissions = can(PERMISSION_ACTIONS.VIEW, [PERMISSION_SUBJECTS.USER_PROFILE_DATA, PERMISSION_SUBJECTS.USER_PROFILE_CONTACT, PERMISSION_SUBJECTS.USER_PROFILE_IDENTITY]) as {
+    USER_PROFILE_DATA: boolean;
+    USER_PROFILE_CONTACT: boolean;
+    USER_PROFILE_IDENTITY: boolean;
+  };
   const { user } = useUserProfileCxt();
   const identity = user?.country?.id === user?.company?.country_id;
 
   return (
     <div className="flex flex-col gap-6">
-      <PersonalDataSectionPersonalForm />
+      {permissions.USER_PROFILE_DATA && <PersonalDataSectionPersonalForm />}
+      {permissions.USER_PROFILE_CONTACT && (
+        <ConnectionDataSectionPersonalForm />
+      )}
 
-      <ConnectionDataSectionPersonalForm />
-
-      {identity && <IdentityDataSectionPersonalForm />}
-      {!identity && <PassportDataSectionPersonalForm />}
+      {identity ? (
+        <>
+          {permissions.USER_PROFILE_IDENTITY && <IdentityDataSectionPersonalForm />}
+        </>
+      ) : (
+        <PassportDataSectionPersonalForm />
+      )}
     </div>
   );
 }
