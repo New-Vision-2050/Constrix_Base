f5de66b6b93824eadb93553e34db2e567e2e1f92
Omar Taha <omartahads@gmail.com>
2025-07-15 10:06:18 +0300

    handle ACL view for ORGANIZATION_JOB_TITLE, ORGANIZATION_JOB_TYPE, ORGANIZATION_MANAGEMENT and ORGANIZATION_DEPARTMENT

--- modules/organizational-structure/constants/organizational-structure-settings.tsx
+++ modules/organizational-structure/constants/organizational-structure-settings.tsx
@@ -4,36 +4,48 @@ import OrgStructureJobTitlesSetting from "../components/organizational-structure
 import OrgStructureJobTypesSetting from "../components/organizational-structure-tabs/organizational-structure-settings/components/org-structure-job-types";
 import OrgStructureManagementSetting from "../components/organizational-structure-tabs/organizational-structure-settings/components/org-structure-managements-settings";
 import OrgStructureDepartmentSetting from "../components/organizational-structure-tabs/organizational-structure-settings/components/org-structure-departments-settings";
+import { can } from "@/hooks/useCan";
+import { PERMISSION_ACTIONS, PERMISSION_SUBJECTS } from "@/modules/roles-and-permissions/permissions";
 
 export const OrganizationalStructureSettingsTabs = (
   t: (key: string) => string
-): SystemTab[] => [
-  {
-    id: "organizational-structure-settings-job-titles",
-    title: t("jobTitles.title"),
-    icon: <UserIcon />,
-    content: (
-      <>
-        <OrgStructureJobTitlesSetting />
-      </>
-    ),
-  },
-  {
-    id: "organizational-structure-settings-job-types",
-    title: t("jobTypes.title"),
-    icon: <UserIcon />,
-    content: <OrgStructureJobTypesSetting />,
-  },
-  {
-    id: "organizational-structure-settings-management",
-    title: t("managements.title"),
-    icon: <SlidersHorizontal width={18} />,
-    content: <OrgStructureManagementSetting />,
-  },
-  {
-    id: "organizational-structure-settings-departments",
-    title: t("departments.title"),
-    icon: <SlidersVertical width={18} />,
-    content: <OrgStructureDepartmentSetting />,
-  },
-];
+): SystemTab[] => {
+
+    const viewPermission = can(PERMISSION_ACTIONS.VIEW,[PERMISSION_SUBJECTS.ORGANIZATION_JOB_TITLE , PERMISSION_SUBJECTS.ORGANIZATION_MANAGEMENT , PERMISSION_SUBJECTS.ORGANIZATION_DEPARTMENT]) as {
+      ORGANIZATION_JOB_TITLE: boolean;
+      ORGANIZATION_JOB_TYPE: boolean;
+      ORGANIZATION_MANAGEMENT: boolean;
+      ORGANIZATION_DEPARTMENT: boolean;
+    };
+
+  return [
+    ...(viewPermission.ORGANIZATION_JOB_TITLE ? [{
+      id: "organizational-structure-settings-job-titles",
+      title: t("jobTitles.title"),
+      icon: <UserIcon />,
+      content: (
+        <>
+          <OrgStructureJobTitlesSetting />
+        </>
+      ),
+    }] : []),
+    ...(viewPermission.ORGANIZATION_JOB_TYPE ? [{
+      id: "organizational-structure-settings-job-types",
+      title: t("jobTypes.title"),
+      icon: <UserIcon />,
+      content: <OrgStructureJobTypesSetting />,
+    }] : []),
+    ...(viewPermission.ORGANIZATION_MANAGEMENT ? [{
+      id: "organizational-structure-settings-management",
+      title: t("managements.title"),
+      icon: <SlidersHorizontal width={18} />,
+      content: <OrgStructureManagementSetting />,
+    }] : []),
+    ...(viewPermission.ORGANIZATION_DEPARTMENT ? [{
+      id: "organizational-structure-settings-departments",
+      title: t("departments.title"),
+      icon: <SlidersVertical width={18} />,
+      content: <OrgStructureDepartmentSetting />,
+    }] : []),
+  ];
+};
