19f010d80c1f852593677e35410c12ea6684f388
SallamRady <sallamrady@gmail.com>
2025-07-17 13:56:30 +0300

    feat: add attendance status badge component and localization files

--- modules/attendance-departure/components/AttendanceDepartureTable/AttendanceStatusBadge.tsx
+++ modules/attendance-departure/components/AttendanceDepartureTable/AttendanceStatusBadge.tsx
@@ -1,34 +1,36 @@
 import React from "react";
+import { useTranslations } from "next-intl";
 import { useAttendance } from "../../context/AttendanceContext";
 import { AttendanceStatusRecord } from "../../types/attendance";
 import { UN_SPECIFIED } from "../../constants/static-data";
 
 interface AttendanceStatusBadgeProps {
-  status: string; // This will now contain the status from the backend
-  record: AttendanceStatusRecord; // The full attendance record
+  record: AttendanceStatusRecord;
+  status?: string;
 }
 
 /**
  * Component to display the attendance status in the table and open the attendance status dialog when clicked
  */
-const AttendanceStatusBadge: React.FC<AttendanceStatusBadgeProps> = ({
-  status,
+export const AttendanceStatusBadge = ({
   record,
-}) => {
+  status,
+}: AttendanceStatusBadgeProps) => {
   const { openAttendanceStatusDialog } = useAttendance();
+  const t = useTranslations("attendanceDeparture.status");
   let color = "";
   let text = "";
   //is_late
   let _status = "";
-  if (record.is_late === 1) {
-    _status = "late";
-  } else if (record.is_absent === 1) {
+  if (record.is_absent === 1) {
     _status = "absent";
   } else if (record.is_holiday === 1) {
     _status = "holiday";
+  } else if (record.is_late === 1) {
+    _status = "late";
   } else if (
-    record.status == "active" ||
-    record.status == "completed"
+    record.status === "active" ||
+    record.status === "completed"
   ) {
     _status = "present";
   }
@@ -36,23 +38,23 @@ const AttendanceStatusBadge: React.FC<AttendanceStatusBadgeProps> = ({
   // Map the backend status to display text
   switch (_status) {
     case "present":
-      text = "حاضر";
+      text = t("present");
       color = "text-green-500";
       break;
     case "absent":
-      text = "غائب";
+      text = t("absent");
       color = "text-red-500";
       break;
     case "late":
-      text = "متأخر";
+      text = t("late");
       color = "text-yellow-400";
       break;
     case "holiday":
-      text = "اجازه";
-      color = "text-yellow-400";
+      text = t("holiday");
+      color = "text-blue-500";
       break;
     default:
-      text = status || UN_SPECIFIED;
+      text = status || t("unspecified");
       color = "text-gray-400";
       break;
   }
