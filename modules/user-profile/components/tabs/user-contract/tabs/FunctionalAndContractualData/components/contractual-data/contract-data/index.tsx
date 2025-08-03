81ff66eba1c0fd1961857ad7c90070e44a806f86
SallamRady <sallamrady@gmail.com>
2025-08-03 15:24:17 +0300

    set available permissions to FunctionalAndContractualData

--- modules/user-profile/components/tabs/user-contract/tabs/FunctionalAndContractualData/components/contractual-data/contract-data/index.tsx
+++ modules/user-profile/components/tabs/user-contract/tabs/FunctionalAndContractualData/components/contractual-data/contract-data/index.tsx
@@ -2,6 +2,8 @@ import ContractDataFormPreviewMode from "./ContractDataFormPreviewMode";
 import ContractDataFormEditMode from "./ContractDataFormEditMode";
 import { useFunctionalContractualCxt } from "../../../context";
 import TabTemplate from "@/components/shared/TabTemplate/TabTemplate";
+import Can from "@/lib/permissions/client/Can";
+import { PERMISSIONS } from "@/lib/permissions/permission-names";
 
 export default function ContractDataForm() {
   // declare and define component state and vars
@@ -12,12 +14,20 @@ export default function ContractDataForm() {
     <TabTemplate
       title={"عقد العمل"}
       loading={userContractDataLoading}
-      reviewMode={<ContractDataFormPreviewMode contract={userContractData} />}
-      editMode={<ContractDataFormEditMode contract={userContractData} />}
+      reviewMode={
+        <Can check={[PERMISSIONS.profile.contractWork.view]}>
+          <ContractDataFormPreviewMode contract={userContractData} />
+        </Can>
+      }
+      editMode={
+        <Can check={[PERMISSIONS.profile.contractWork.update]}>
+          <ContractDataFormEditMode contract={userContractData} />
+        </Can>
+      }
       settingsBtn={{
         items: [
-          { title: "طلباتي", onClick: () => {} ,disabled:true},
-          { title: "أنشاء طلب", onClick: () => {},disabled:true },
+          { title: "طلباتي", onClick: () => {}, disabled: true },
+          { title: "أنشاء طلب", onClick: () => {}, disabled: true },
         ],
       }}
     />
--- modules/user-profile/components/tabs/user-contract/tabs/FunctionalAndContractualData/components/job-information/index.tsx
+++ modules/user-profile/components/tabs/user-contract/tabs/FunctionalAndContractualData/components/job-information/index.tsx
@@ -1,7 +1,9 @@
+import Can from "@/lib/permissions/client/Can";
 import { useFunctionalContractualCxt } from "../../context";
 import JobInformationEditMode from "./JobInformationEditMode";
 import JobInformationPreviewMode from "./JobInformationPreviewMode";
 import TabTemplate from "@/components/shared/TabTemplate/TabTemplate";
+import { PERMISSIONS } from "@/lib/permissions/permission-names";
 
 export default function JobInformation() {
   const { professionalDataLoading } = useFunctionalContractualCxt();
@@ -12,8 +14,16 @@ export default function JobInformation() {
       <TabTemplate
         title={"بيانات التوظيف"}
         loading={professionalDataLoading}
-        reviewMode={<JobInformationPreviewMode />}
-        editMode={<JobInformationEditMode />}
+        reviewMode={
+          <Can check={[PERMISSIONS.profile.jobOffer.view]}>
+            <JobInformationPreviewMode />
+          </Can>
+        }
+        editMode={
+          <Can check={[PERMISSIONS.profile.jobOffer.update]}>
+            <JobInformationEditMode />
+          </Can>
+        }
       />
     </div>
   );
