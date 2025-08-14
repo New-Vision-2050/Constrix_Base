45cc13895ebf7cdcdc35bb9adf45ae4ec1056cda
RustedMiind <alisolimanworks@gmail.com>
2025-08-14 15:55:03 +0300

    feat: update permission handling across multiple pages and components

--- app/[locale]/(main)/(program-settings)/users-settings/page.tsx
+++ app/[locale]/(main)/(program-settings)/users-settings/page.tsx
@@ -1,18 +1,21 @@
 import TabsGroup from "@/components/shared/TabsGroup";
 import { UsersSettingsTab } from "@/modules/program-settings/users-settings/components/Tabs";
 import React from "react";
+import { PERMISSIONS } from "@/lib/permissions/permission-names";
+import withServerPermissionsPage from "@/lib/permissions/server/withServerPermissionsPage";
 
-const UsersSettingsPage = () => {
-  return (
-    <div className="px-8 space-y-7">
-      <TabsGroup
-        tabs={UsersSettingsTab}
-        defaultValue="main"
-        variant="primary"
-        tabsListClassNames="justify-start gap-10"
-      />
-    </div>
-  );
-};
-
-export default UsersSettingsPage;
+export default withServerPermissionsPage(
+  function UsersSettingsPage() {
+    return (
+      <div className="px-8 space-y-7">
+        <TabsGroup
+          tabs={UsersSettingsTab}
+          defaultValue="main"
+          variant="primary"
+          tabsListClassNames="justify-start gap-10"
+        />
+      </div>
+    );
+  },
+  [Object.values(PERMISSIONS.subEntity)]
+);
