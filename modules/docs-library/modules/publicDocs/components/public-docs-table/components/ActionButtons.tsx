03a6aaf3f2829d71367f1bba8766e3c7cfc5b909
SallamRady <sallamrady@gmail.com>
2025-10-08 16:26:25 +0300

    feat: implement file/directory deletion with confirmation dialog and UI updates

--- modules/docs-library/modules/publicDocs/components/public-docs-table/components/ActionButtons.tsx
+++ modules/docs-library/modules/publicDocs/components/public-docs-table/components/ActionButtons.tsx
@@ -6,6 +6,7 @@ import {
   Share2,
   Edit,
   Trash2,
+  EyeClosed,
 } from "lucide-react";
 import {
   DropdownMenu,
@@ -17,6 +18,10 @@ import {
 import { useTranslations } from "next-intl";
 import { usePublicDocsCxt } from "../../../contexts/public-docs-cxt";
 import { DocumentT } from "../../../types/Directory";
+import ConfirmationDialog from "@/components/shared/ConfirmationDialog";
+import { useState } from "react";
+import { apiClient, baseURL } from "@/config/axios-config";
+import { toast } from "sonner";
 
 /**
  * Action buttons component
@@ -29,13 +34,41 @@ interface ActionButtonsProps {
 export const ActionButtons = ({ document }: ActionButtonsProps) => {
   const isDirectory = !Boolean(document.reference_number);
   const t = useTranslations("docs-library.publicDocs.table.actions");
-  const { setOpenDirDialog, setOpenFileDialog, setEditedDoc } =
-    usePublicDocsCxt();
+  const [openDelete, setOpenDelete] = useState(false);
+  const {
+    setOpenDirDialog,
+    setOpenFileDialog,
+    setEditedDoc,
+    refetchDocs,
+    selectedDocument,
+    storeSelectedDocument,
+  } = usePublicDocsCxt();
+
+  // check current doc in details
+  const inDetails = selectedDocument && selectedDocument?.id == document.id;
+
+  const handleDelete = async () => {
+    try {
+      const _url =
+        baseURL +
+        (isDirectory ? `/folders/${document?.id}` : `/files/${document?.id}`);
+      await apiClient.delete(_url);
+
+      toast.success(t("deleteSuccess"));
+      setOpenDelete(false);
+      refetchDocs();
+    } catch (error) {
+      toast.error(t("deleteFailed"));
+    }
+  };
 
   const handleAction = (action: string) => {
     // TODO: Implement actual action handlers
     switch (action) {
       case "view":
+        if (selectedDocument?.id == document.id)
+          storeSelectedDocument(undefined);
+        else storeSelectedDocument(document);
         break;
       case "download":
         break;
@@ -47,6 +80,7 @@ export const ActionButtons = ({ document }: ActionButtonsProps) => {
         else setOpenFileDialog(true);
         break;
       case "delete":
+        setOpenDelete(true);
         break;
       default:
         break;
@@ -54,57 +88,72 @@ export const ActionButtons = ({ document }: ActionButtonsProps) => {
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="secondary" size="sm" className="p-2 hover:bg-muted">
-          {t("title")}
-          <MoreHorizontal className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" size="sm" className="p-2 hover:bg-muted">
+            {t("title")}
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent className="w-44">
-        <DropdownMenuItem
-          onClick={() => handleAction("view")}
-          className="flex items-center gap-2"
-        >
-          <Eye className="h-4 w-4" />
-          {t("view")}
-        </DropdownMenuItem>
+        <DropdownMenuContent className="w-44">
+          <DropdownMenuItem
+            onClick={() => handleAction("view")}
+            className="flex items-center gap-2"
+          >
+            {inDetails ? (
+              <EyeClosed className="h-4 w-4" />
+            ) : (
+              <Eye className="h-4 w-4" />
+            )}
+            {inDetails ? t("hide") : t("view")}
+          </DropdownMenuItem>
 
-        <DropdownMenuItem
-          onClick={() => handleAction("download")}
-          className="flex items-center gap-2"
-        >
-          <Download className="h-4 w-4" />
-          {t("download")}
-        </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => handleAction("download")}
+            className="flex items-center gap-2"
+          >
+            <Download className="h-4 w-4" />
+            {t("download")}
+          </DropdownMenuItem>
 
-        <DropdownMenuItem
-          onClick={() => handleAction("share")}
-          className="flex items-center gap-2"
-        >
-          <Share2 className="h-4 w-4" />
-          {t("share")}
-        </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => handleAction("share")}
+            className="flex items-center gap-2"
+          >
+            <Share2 className="h-4 w-4" />
+            {t("share")}
+          </DropdownMenuItem>
 
-        <DropdownMenuSeparator />
+          <DropdownMenuSeparator />
 
-        <DropdownMenuItem
-          onClick={() => handleAction("edit")}
-          className="flex items-center gap-2"
-        >
-          <Edit className="h-4 w-4" />
-          {t("edit")}
-        </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => handleAction("edit")}
+            className="flex items-center gap-2"
+          >
+            <Edit className="h-4 w-4" />
+            {t("edit")}
+          </DropdownMenuItem>
 
-        <DropdownMenuItem
-          onClick={() => handleAction("delete")}
-          className="flex items-center gap-2 text-destructive focus:text-destructive"
-        >
-          <Trash2 className="h-4 w-4" />
-          {t("delete")}
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem
+            onClick={() => handleAction("delete")}
+            className="flex items-center gap-2 text-destructive focus:text-destructive"
+          >
+            <Trash2 className="h-4 w-4" />
+            {t("delete")}
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <ConfirmationDialog
+        open={openDelete}
+        onClose={() => {
+          setOpenDelete(false);
+        }}
+        onConfirm={handleDelete}
+        description={isDirectory ? t("deleteDir") : t("deleteFile")}
+        showDatePicker={false}
+      />
+    </>
   );
 };
