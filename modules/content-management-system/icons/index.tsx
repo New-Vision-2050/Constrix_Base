b8cc8f6b79ca8f24257faf81d121c0e5810bb3ee
SallamRady <sallamrady@gmail.com>
2025-11-17 17:09:43 +0300

    feat: implement icon management dialog and localization updates
    - Added SetIconDialog component for creating and editing icons with localized fields in Arabic and English.
    - Enhanced CMSIconsModule to include a dialog trigger for adding icons and improved edit functionality.
    - Updated localization files for Arabic and English to include new labels and messages for icon management.
    - Introduced form validation for icon data, ensuring required fields and minimum lengths for names and image uploads.

--- modules/content-management-system/icons/index.tsx
+++ modules/content-management-system/icons/index.tsx
@@ -3,20 +3,40 @@ import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import IconsGrid from "./components/IconsGrid";
+import DialogTrigger from "@/components/headless/dialog-trigger";
+import { useState } from "react";
+import SetIconDialog from "./components/SetIconDialog";
 
 export default function CMSIconsModule() {
     const t = useTranslations("content-management-system.icons");
+    const [editingIconId, setEditingIconId] = useState<string | null>(null);
+
+    const OnEditIcon = (id: string) => {
+        setEditingIconId(id);
+    }
 
     return <div className="px-6 py-2 flex flex-col gap-4">
         {/* title & add action */}
         <div className="flex items-center justify-between">
             <h1 className="text-2xl font-bold">{t("title")}</h1>
-            <Button>
-                <PlusIcon />
-                {t("addIcon")}
-            </Button>
+            <DialogTrigger
+                component={SetIconDialog}
+                dialogProps={{ onSuccess: () => { } }}
+                render={({ onOpen }) => (
+                    <Button onClick={onOpen}>
+                        <PlusIcon />
+                        {t("addIcon")}
+                    </Button>
+                )}
+            />
         </div>
         {/* icons grid */}
-        <IconsGrid OnEdit={()=>{}} />
+        <IconsGrid OnEdit={OnEditIcon} />
+        <SetIconDialog
+            open={Boolean(editingIconId)}
+            onClose={() => setEditingIconId(null)}
+            iconId={editingIconId || undefined}
+            onSuccess={() => { }}
+        />
     </div>
 }
