1aaa9703e69ca42de0d55b2b5b983372fa63a518
RustedMiind <alisolimanworks@gmail.com>
2025-11-20 09:09:44 +0200

    add test log statement to ContentManagementSystemMainDataPage

--- app/[locale]/(main)/content-management-system/mainData/page.tsx
+++ app/[locale]/(main)/content-management-system/mainData/page.tsx
@@ -1,5 +1,7 @@
 import ContentManagementSystemMainData from "@/modules/content-management-system/mainData";
 
 export default function ContentManagementSystemMainDataPage() {
+  const test = "Testing a dummy PR";
+  console.log(test);
   return <ContentManagementSystemMainData />;
 }
