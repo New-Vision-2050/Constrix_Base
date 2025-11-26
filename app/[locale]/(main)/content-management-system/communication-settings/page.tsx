b148eba99f3155cb1b427501c80d587677cb90f4
SallamRady <sallamrady@gmail.com>
2025-11-26 20:02:26 +0300

    feat: add permission checks to Communication Settings page for enhanced access control

--- app/[locale]/(main)/content-management-system/communication-settings/page.tsx
+++ app/[locale]/(main)/content-management-system/communication-settings/page.tsx
@@ -1,5 +1,11 @@
+import withPermissionsPage from "@/lib/permissions/client/withPermissionsPage";
+import { PERMISSIONS } from "@/lib/permissions/permission-names";
 import CommunicationSettingsView from "@/modules/content-management-system/communication-settings";
 
-export default function CommunicationSettingsPage() {
+function CommunicationSettingsPage() {
     return <CommunicationSettingsView />;
 }
+
+export default withPermissionsPage(CommunicationSettingsPage, [
+    Object.values(PERMISSIONS.CMS.communicationSettings).flatMap((p) => Object.values(p)),
+]);
