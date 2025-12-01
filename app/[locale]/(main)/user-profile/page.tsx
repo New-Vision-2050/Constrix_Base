be21aa3ea037057a73f0b4fd76ce3909518a8214
SallamRady <sallamrady@gmail.com>
2025-12-01 14:13:58 +0300

    feat: update UserProfilePage to accept search parameters and pass them to UserProfileModule
    - Modified UserProfilePage to extract userId and companyId from searchParams.
    - Updated UserProfileModule to accept userId and companyId as props for enhanced functionality.

--- app/[locale]/(main)/user-profile/page.tsx
+++ app/[locale]/(main)/user-profile/page.tsx
@@ -2,9 +2,16 @@ import UserProfileModule from "@/modules/user-profile/views";
 import { PERMISSIONS } from "@/lib/permissions/permission-names";
 import withServerPermissionsPage from "@/lib/permissions/server/withServerPermissionsPage";
 
+
+type UserProfilePageProps = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
 export default withServerPermissionsPage(
-  function UserProfilePage() {
-    return <UserProfileModule />;
+  async function UserProfilePage({ searchParams }: UserProfilePageProps) {
+    const userId = searchParams.id as string;
+    const companyId = searchParams.company_id as string;
+    return <UserProfileModule userId={userId} companyId={companyId} />;
   },
   [
     [
--- modules/user-profile/views/index.tsx
+++ modules/user-profile/views/index.tsx
@@ -3,7 +3,8 @@
 import { UserProfileCxtProvider } from "../context/user-profile-cxt";
 import UserProfileEntryPoint from "../components/entry-point";
 
-export default function UserProfileModule() {
+export default function UserProfileModule({ userId, companyId }: { userId: string, companyId: string }) {
+  console.log("searchParams1010", userId, companyId);
   return (
     <UserProfileCxtProvider>
       <UserProfileEntryPoint />
