b8872d2b226a5a9861bc2a93254505525c6f11a6
SallamRady <sallamrady@gmail.com>
2026-01-06 17:27:26 +0300

    feat: add localization support for legal data form with add and edit translations

---
+++ messages/groups/company-profile/legal-data-form.ts
@@ -0,0 +1,6 @@
+import { _m, MessagesGroup } from "../../types";
+
+export const companyProfileLegalDataFormMessages = new MessagesGroup({
+  addLegalData: _m("Add Legal Data", "اضافة بيان قانوني"),
+  editLegalData: _m("Edit Legal Data", "تعديل بيان قانوني"),
+});
--- modules/company-profile/components/official-data/legal-data-section/set-legal-data/index.tsx
+++ modules/company-profile/components/official-data/legal-data-section/set-legal-data/index.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { useTranslations } from "next-intl";
+
 export default function SetLegalDataForm() {
+    // get Translation
+    const t = useTranslations('companyProfileLegalDataForm');
     return <div>SetLegalData</div>;
 }
