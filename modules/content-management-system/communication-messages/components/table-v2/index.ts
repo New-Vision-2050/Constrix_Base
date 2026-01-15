4bf86b1be815190a64e4979acbe5969e9cdd59d9
SallamRady <sallamrady@gmail.com>
2026-01-15 13:45:26 +0300

    refactor: remove table-v2 components index file from communication messages module
    - Deleted index.ts barrel export file from table-v2 components directory
    - Removed exports for createColumns, TableFilters, RowActions, and useTableData
    - Components will now be imported directly from their respective files instead of through barrel exports

--- modules/content-management-system/communication-messages/components/table-v2/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * V2 Components Exports
- * Modular components for the new HeadlessTable implementation
- */
-export { createColumns } from "./columns";
-export { TableFilters } from "./filters";
-export { RowActions } from "./actions";
-export { useTableData } from "./use-table-data";
