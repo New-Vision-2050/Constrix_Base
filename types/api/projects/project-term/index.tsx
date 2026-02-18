21ce50ca0b13006c824602ff62f4f27c16e6d75d
Mohammed AbdulLatif <mah01010533395@gmail.com>
2026-02-19 00:55:59 +0300

    feat_Project_terms

---
+++ types/api/projects/project-term/index.tsx
@@ -0,0 +1,30 @@
+import { DB_Boolean } from "../../shared/db-boolean";
+
+export interface PRJ_ProjectTerm {
+  id: number;
+  reference_number: string;
+  name: string;
+  description: string;
+  sub_items_count: number;
+  services: string[];
+  status: DB_Boolean;
+  parent_id: number | null;
+  project_type_id: number | null;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface PRJ_ProjectTermWithServices extends PRJ_ProjectTerm {
+  services: PRJ_ProjectTermService[];
+}
+
+export interface PRJ_ProjectTermService {
+  id: number;
+  project_term_id: number;
+  service_name: string;
+  service_type: string;
+  cost: number;
+  unit: string;
+  quantity: number;
+  status: DB_Boolean;
+}
