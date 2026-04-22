b5b68be7cec4ae0e6110a038e2456f22ff050414
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-04-22 17:55:28 +0300

    pull

--- components/shared/LocationSelector.tsx
+++ components/shared/LocationSelector.tsx
@@ -130,7 +130,7 @@ const LocationSelector: React.FC<LocationSelectorProps> = ({
   // Load Google Maps API
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey:  "AIzaSyCUlTmZo8cqfmOQgBqqiFbP3QyHYhHy_hQ",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
     libraries,
   });
 
