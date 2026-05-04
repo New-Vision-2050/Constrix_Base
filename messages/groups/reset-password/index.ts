6b835711dde5dc8bf8309d527fa6e2cfd3bf3b09
Mohammed AbdulLatif <mah01010533395@gmail.com>
2026-05-04 13:44:37 +0300

    add a page to change the password

--- messages/groups/reset-password/index.ts
+++ messages/groups/reset-password/index.ts
@@ -5,5 +5,7 @@ export const resetPasswordMessages = new MessagesGroup({
   NewPassword: _m("New Password", "كلمة المرور الجديدة"),
   ConfirmNewPassword: _m("Confirm New Password", "تأكيد كلمة المرور الجديدة"),
   PasswordRequirements: _m("Password must be at least 8 characters, contain at least one uppercase letter, and one special character.", "يجب أن تكون كلمة المرور بطول 8 أحرف على الأقل، حرف كبير واحد على الأقل، استخدام رمز خاص واحد على الأقل."),
-  Confirm: _m("Confirm", "تأكيد")
+  Confirm: _m("Confirm", "تأكيد"),
+  GenericError: _m("An error occurred, please try again", "حدث خطأ، حاول مرة أخرى"),
+  Success: _m("Password changed successfully", "تم تغيير كلمة المرور بنجاح"),
 });
