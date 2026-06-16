c548c265ae9b5b4a57789e153aa6f9122f846f95
Abdel-Rahman112000 <abdelrahman30688@gmail.com>
2026-06-16 15:38:58 +0300

    Add validation messages and edit title support in CreateNewFileDialog

--- modules/docs-library/modules/publicDocs/views/public-docs-tab/create-new-file/CreateNewFileDialog.tsx
+++ modules/docs-library/modules/publicDocs/views/public-docs-tab/create-new-file/CreateNewFileDialog.tsx
@@ -20,7 +20,7 @@ export default function CreateNewFileDialog({ open, onClose }: PropsType) {
   const isRtl = lang === "ar";
   // translate
   const t = useTranslations("docs-library.publicDocs.createNewFileDialog");
-  const { setEditedDoc } = usePublicDocsCxt();
+  const { setEditedDoc, editedDoc } = usePublicDocsCxt();
 
   const handleOpenChange = (isOpen: boolean) => {
     if (!isOpen) {
@@ -34,9 +34,11 @@ export default function CreateNewFileDialog({ open, onClose }: PropsType) {
       <DialogContent className="max-md w-full" dir={isRtl ? "rtl" : "ltr"}>
         <DialogHeader>
           <DialogTitle className="text-center text-xl font-semibold text-foreground">
-            {t("title")}
+            {editedDoc ? t("editTitle") : t("title")}
           </DialogTitle>
-          <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+          <DialogClose
+            className={`absolute top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground ${isRtl ? "left-4" : "right-4"}`}
+          >
             <X className="w-5 h-5 text-foreground" />
             <span className="sr-only">Close</span>
           </DialogClose>
