cce21c91df8eeac31942f572cfddee2e940e57bb
Begad-El3shry <89875491+Begad-El3shry@users.noreply.github.com>
2026-06-29 16:50:48 +0300

    Add save attachment notes (#1378)

--- messages/groups/projects/index.ts
+++ messages/groups/projects/index.ts
@@ -139,7 +139,7 @@ export const projectMessages = new MessagesGroup({
     attachFile: _m("Attach File", "ارفاق ملف"),
     notes: _m("Notes", "ملاحظات"),
     save: _m("Save", "حفظ"),
-    saveWithNotes: _m("Approve with notes", "اعتماد بملاحظات"),
+    saveWithNotes: _m("Save with notes", "حفظ بملاحظات"),
     saveViewerChangesSuccess: _m(
       "Document changes saved",
       "تم حفظ تغييرات المستند",
